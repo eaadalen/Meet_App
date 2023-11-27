6bf2a2e06d6c2680ca3443c61196424adcb66c01
eaadalen <eaadalen112@gmail.com>
2023-11-27 10:47:15 -0600

    4.3 revisions

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://eaadalen.github.io/meet/"
+  "https://eaadalen.github.io/Meet_App/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
