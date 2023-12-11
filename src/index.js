19112016d9e3446a85c2084b987971ac82e4c3ba
eaadalen <eaadalen112@gmail.com>
2023-12-10 19:12:52 -0600

    4.7 revisions
    Removed intentional error

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('41b96f9c5bb841eb9657ec4c027485ec').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
