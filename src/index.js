8409c9d64c4d76bd6574f45ca2954c9120ccec8a
eaadalen <eaadalen112@gmail.com>
2023-12-13 17:26:50 -0600

    4.9 revisions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import * as atatus from 'atatus-spa';
 atatus.config('41b96f9c5bb841eb9657ec4c027485ec').install();
@@ -18,7 +19,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
